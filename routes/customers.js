9075fcd1e8ff5bd4bca9e9cc5b13bbe513ebc6c1
Cambara <tiago.cambara21@gmail.com>
2014-12-06 01:47:49 -0200

    add the routes, views and modulos

---
+++ routes/customers.js
@@ -0,0 +1,51 @@
+/*
+	GET customers listing
+*/
+
+exports.list = function(req, res) {
+	req.getConnection(function(err,connection){
+
+		connection.query('SELECT * FROM customer',function(err,rows) {
+			if(err)
+				console.log('Error Selecting : %s',err);
+
+			res.render('./customer/list',{page_title:"Customers - Node.js", data:rows});
+			});
+		});
+};
+
+exports.add = function(req,res) {
+	res.render('./customer/add_customer',{page_title:'Add Customers-Node.js'});	
+};
+
+// exports.edit = function(req,res) {
+// 	var id = req.params.id;
+
+// 	req.getConnection(function(err,connection) {
+// 		connection.query('SELECT * FROM customer WHERE id = ?',[id],function(err,rows) 
+// 		{
+// 			if(err)
+// 				console.log("Error Selecting : %s",err);
+// 			res.render("edit_customer",{page_title:"Edit Customers -Node.js",data:rows});
+
+// 		});
+// 	});
+// };
+
+exports.save = function(req,res) {
+	var input = JSON.parse(JSON.stringify(req.body));
+	req.getConnection(function(err, connection) 
+	{
+		var data = {
+			name : input.name,
+			last_name : input.last_name,
+			birth_date : input.birth_date
+		};
+
+		var query = connection.query("INSERT INTO customer set ?",data,function(err,rows) 
+		{
+			if (err) { console.log("Error inserting : %s",err);};
+			res.redirect('/customers');
+		});
+	});
+}
---
+++ routes/error.js
@@ -0,0 +1,5 @@
+exports.not_found = function(req, res){
+
+	res.render('./error/404',{page_title:"404"});
+
+};
